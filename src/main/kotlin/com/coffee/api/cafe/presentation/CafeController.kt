c3e85061414b59364eb73d1ecc4fa525ed761ba7
LEEJaeHyeok97 <hazardous10@naver.com>
2025-01-09 14:11:28 +0900

    docs : swagger 코드 분리

--- src/main/kotlin/com/coffee/api/cafe/presentation/CafeController.kt
+++ src/main/kotlin/com/coffee/api/cafe/presentation/CafeController.kt
@@ -1,6 +1,7 @@
 package com.coffee.api.cafe.presentation
 
 import com.coffee.api.cafe.application.usecase.FindCafe
+import com.coffee.api.cafe.presentation.docs.CafeApi
 import com.coffee.api.common.presentation.response.ApiResponse
 import com.coffee.api.common.presentation.response.ApiResponseGenerator
 import io.swagger.v3.oas.annotations.Operation
@@ -13,16 +14,14 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
-@Tag(name = "Cafe", description = "Cafe API")
 @RestController
 @RequestMapping("/api/v1/cafes")
 class CafeController(
     val findCafe: FindCafe,
-) {
+): CafeApi {
 
-    @Operation(summary = "모든 카페 조회", description = "모든 카페를 조회합니다.")
     @GetMapping
-    fun findAllCafes(
+    override fun findAllCafes(
         @RequestParam(value = "lastCafeId", required = false) lastCafeId: UUID?
     ): ApiResponse<ApiResponse.SuccessBody<FindCafe.Result?>> {
         val result = findCafe.execute(FindCafe.Query(lastCafeId))
---
+++ src/main/kotlin/com/coffee/api/cafe/presentation/docs/CafeApi.kt
@@ -0,0 +1,17 @@
+package com.coffee.api.cafe.presentation.docs
+
+import com.coffee.api.cafe.application.usecase.FindCafe
+import com.coffee.api.common.presentation.response.ApiResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.RequestParam
+import java.util.*
+
+@Tag(name = "Cafe", description = "Cafe API")
+interface CafeApi {
+
+    @Operation(summary = "모든 카페 조회", description = "모든 카페를 조회합니다.")
+    fun findAllCafes(
+        @RequestParam(value = "lastCafeId", required = false) lastCafeId: UUID?
+    ): ApiResponse<ApiResponse.SuccessBody<FindCafe.Result?>>
+}
