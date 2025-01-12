05cce755a9038ac6040a1c17336fe203540591fc
mando <201566@jnu.ac.kr>
2025-01-12 21:51:45 +0900

    feat : cors 추가

---
+++ src/main/kotlin/com/coffee/api/config/WebMvcConfig.kt
@@ -0,0 +1,34 @@
+package com.coffee.api.config
+
+import io.swagger.v3.oas.models.PathItem
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebMvcConfig(
+    @Value("\${cors.allow-origin.urls}")
+    private val allowOriginUrlPatterns: String
+) : WebMvcConfigurer {
+
+    private val allowOriginUrls: List<String> = allowOriginUrlPatterns
+        .split(",")
+        .map(String::trim)
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**")
+            .allowedOriginPatterns(*allowOriginUrls.toTypedArray())
+            .allowedMethods(*ALLOWED_METHODS.toTypedArray())
+            .exposedHeaders("Authorization", "Set-Cookie")
+            .allowCredentials(true)
+            .maxAge(MAX_AGE_SECS)
+    }
+
+    companion object {
+        private const val MAX_AGE_SECS: Long = 3600
+        private val ALLOWED_METHODS = PathItem.HttpMethod.entries.map { it.name }
+    }
+}
