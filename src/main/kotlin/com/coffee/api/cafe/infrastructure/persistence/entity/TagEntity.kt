a3231159bbe4e35b0cfe416e4cebe14e3713e423
LEEJaeHyeok97 <hazardous10@naver.com>
2025-01-13 21:53:54 +0900

    chore : import 구문 정리

--- src/main/kotlin/com/coffee/api/cafe/infrastructure/persistence/entity/TagEntity.kt
+++ src/main/kotlin/com/coffee/api/cafe/infrastructure/persistence/entity/TagEntity.kt
@@ -1,7 +1,10 @@
 package com.coffee.api.cafe.infrastructure.persistence.entity
 
 import com.coffee.api.common.infrastructure.persistence.BaseEntity
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 import java.util.UUID
 
 @Entity
