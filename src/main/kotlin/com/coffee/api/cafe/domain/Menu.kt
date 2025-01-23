948cfbc507629ed45f8d8a70710674367d46e670
kssumin <201566@jnu.ac.kr>
2025-01-24 01:20:56 +0900

    feat : menu name 칼럼 추가

--- src/main/kotlin/com/coffee/api/cafe/domain/Menu.kt
+++ src/main/kotlin/com/coffee/api/cafe/domain/Menu.kt
@@ -7,37 +7,41 @@ import java.util.UUID
 
 class Menu private constructor(
     override val id: Id,
+    val name: String,
     val cafe: Cafe,
     val price: Int,
     val imageUrl: String,
-    val description: String
+    val description: String,
 ) : AbstractDomain<Menu, Menu.Id>() {
 
     companion object {
         @JsonCreator
         fun create(
             id: UUID,
+            name: String,
             cafe: Cafe,
             price: Int,
             imageUrl: String,
-            description: String
+            description: String,
         ): Menu {
             return Menu(
                 id = UUIDTypeId.from(id),
+                name = name,
                 cafe = cafe,
                 price = price,
                 imageUrl = imageUrl,
-                description = description
+                description = description,
             )
         }
 
         operator fun invoke(
             id: UUID,
+            name: String,
             cafe: Cafe,
             price: Int,
             imageUrl: String,
-            description: String
-        ): Menu = create(id, cafe, price, imageUrl, description)
+            description: String,
+        ): Menu = create(id, name, cafe, price, imageUrl, description)
     }
 
     data class Id(override val value: UUID) : UUIDTypeId(value)
