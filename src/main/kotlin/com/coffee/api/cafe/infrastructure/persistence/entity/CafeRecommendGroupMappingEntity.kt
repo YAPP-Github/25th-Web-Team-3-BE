452d6e1cc6a5530c4938d41d3e9e107ee71a9a4c
mando <mando@krafton.com>
2025-01-28 22:08:59 +0900

    feat : fk 제약조건 제거 및 해당 칼럼에 인덱스 생성

--- src/main/kotlin/com/coffee/api/cafe/infrastructure/persistence/entity/CafeRecommendGroupMappingEntity.kt
+++ src/main/kotlin/com/coffee/api/cafe/infrastructure/persistence/entity/CafeRecommendGroupMappingEntity.kt
@@ -5,14 +5,20 @@ import jakarta.persistence.*
 import java.util.*
 
 @Entity
-@Table(name = "cafe_recommend_group_mappings")
+@Table(
+    name = "cafe_recommend_group_mappings",
+    indexes = [
+        Index(name = "idx_cafe_recommend_group_mapping_cafe_id", columnList = "cafe_id"),
+        Index(name = "idx_cafe_recommend_group_mapping_group_id", columnList = "group_id")
+    ]
+)
 class CafeRecommendGroupMappingEntity(
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cafe_id")
+    @JoinColumn(name = "cafe_id", foreignKey = ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
     var cafe: CafeEntity,
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "group_id")
+    @JoinColumn(name = "group_id", foreignKey = ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
     var recommendGroup: CafeRecommendGroupEntity,
 
     @Id
