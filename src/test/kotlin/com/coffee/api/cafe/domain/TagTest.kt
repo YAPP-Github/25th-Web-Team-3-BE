6fac07172242828facd1a4c063447f3a0c2dcf86
LeeJaeHyeok97 <hazardous10@naver.com>
2025-04-10 15:21:28 +0900

    [TEST] Tag 도메인 테스트 코드 추가

--- src/test/kotlin/com/coffee/api/cafe/domain/TagTest.kt
+++ src/test/kotlin/com/coffee/api/cafe/domain/TagTest.kt
@@ -1,5 +1,28 @@
 package com.coffee.api.cafe.domain
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.util.UUID
 
-class TagTest
+class TagTest {
+
+    @DisplayName("태그 정보로 태그를 생성한다")
+    @Test
+    fun create() {
+        // given
+        val id = UUID.randomUUID()
+        val name = "brand"
+        val imageUrl = "www.imageUrl.xzy/image"
+        val priority = 1.1f
+
+        // when
+        val tag = Tag.create(id, name, imageUrl, priority)
+
+        // then
+        assertThat(tag.id).isNotNull()
+        assertThat(tag)
+            .extracting("name", "imageUrl", "priority")
+            .contains("brand", "www.imageUrl.xzy/image", 1.1f)
+    }
+}
